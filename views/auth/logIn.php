1f9c455a91471aa1e1fe8c35eab8ac2c5ab1cdd9
altimos <altimos@example.com>
2020-02-10 21:57:54 +0200

    Make the logIn view

---
+++ views/auth/logIn.php
@@ -0,0 +1,10 @@
+<?php
+
+
+?>
+
+<form action = "../authenticate" method = "POST">
+    Name: <input type="text" name="name" required/>
+    Password: <input type="password" name="pw" required/>
+    <button type="submit"> Login </button>
+</form>
--- views/auth/reg.php
+++ views/auth/reg.php
@@ -1,9 +1,8 @@
 <?php
 
-
 ?>
 
-<form action = <?php echo _PUBLIC . "/auth/newUser"; ?> method="POST">
+<form action = <?php echo _PUBLIC . "auth/newUser"; ?> method="POST">
     Name: <input type="text" name="name" required/>
     Email: <input type="text" name="email" required/>
     Password: <input type="password" name="pw" required/>

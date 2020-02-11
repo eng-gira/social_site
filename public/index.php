7659962594ee79e102059b052e5ad1831c722691
altimos <altimos@example.com>
2020-02-11 21:26:09 +0200

    Added styles and some HTML5

--- public/index.php
+++ public/index.php
@@ -21,6 +21,9 @@
 
     //AUTO LOAD
     spl_autoload_register('spl_autoload', false);
-
+?>
+    <link rel="stylesheet" href="/ekom/inc/css/styles.css">
+    <div id="test1"> Test1 </div>
+<?php
     new Application;
 ?>
--- views/home/index.php
+++ views/home/index.php
@@ -1,12 +1,14 @@
 <html>
     <head> <title> Home | Ekom </title> </head>
     <body>
-        <div id = "nav">
+        <nav>
             <a href="auth/reg"> Register </a>  |  <a href="auth/logIn"> Log In </a>
-        </div>
-        <h1> Index View </h1>
-        <h3> Available Data: </h3>
-        <h4> Id: <?php echo $this->getData()['id']; ?> </h4>
-        <h4> Name: <?php echo $this->getData()['name']; ?> </h4>
+        </nav>
+        <main>
+            <h1> Index View </h1>
+            <h3> Available Data: </h3>
+            <h4> Id: <?php echo $this->getData()['id']; ?> </h4>
+            <h4> Name: <?php echo $this->getData()['name']; ?> </h4>
+        </main>
     </body>
 </html>

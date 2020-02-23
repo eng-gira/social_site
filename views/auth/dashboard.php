5ceaf4554ed65b8b2ce563b1aa830855cef6c403
eng-gira <58122030+eng-gira@users.noreply.github.com>
2020-02-23 03:21:59 +0200

    Testing pull

--- views/auth/dashboard.php
+++ views/auth/dashboard.php
@@ -1,12 +1,6 @@
 <?php
-    /*echo "Welcome you, authenticated person! This is Your Dashboard<br>";
-    if(strlen($this->getData()['unm'])>0)
-    {
-        $username=$this->getData()['unm'];
-        echo "Your username is: " . $username . "<br>";
-    }
 
-    echo $_SESSION['username'] . "<- from sessions. <br>";*/
+  //  echo $_SESSION['username'] . "<- from sessions. <br>";*/
     
 
 ?>

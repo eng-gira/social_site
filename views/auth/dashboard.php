c8ff82ed7b7508cde2efab353253284406869363
altimos <altimos@example.com>
2020-03-22 21:54:05 +0200

    Bug Fixed: show posts with their comments

--- views/auth/dashboard.php
+++ views/auth/dashboard.php
@@ -21,6 +21,7 @@
 
 
             <?php
+            
                 $id_visited= $this->getData()['id_visited'][0];
                 $f_unf = $this->getData()['id_visited'][1];
                 if($id_visited!=$_SESSION['id'])
@@ -98,9 +99,9 @@
                     <a href=<?php echo "/ekom/public/post/editPost/" . $arr_my_posts[$i]['id']; ?>> Edit </a> | 
                     <button onclick='confirmPostDeletion("<?php echo $delete_link; ?>")')>Delete</button>
                     
-                    <p id=<?php echo 'upvote_post_'.$current_post_id;?> style='cursor:pointer' 
+                    <p id=<?php echo 'upvote_post_'.$current_post_id;?> style='cursor:pointer;width:50px' 
                         onclick='<?php echo $upvote_post_js_func; ?>'><?php echo $upvoted?'upvoted':'upvote'; ?> </p>
-                    <p id=<?php echo 'downvote_post_'.$current_post_id;?> style='cursor:pointer' 
+                    <p id=<?php echo 'downvote_post_'.$current_post_id;?> style='cursor:pointer;width:50px' 
                         onclick='<?php echo $downvote_post_js_func; ?>'><?php echo $downvoted?'downvoted':'downvote'; ?></p>
                     
                     <?php
@@ -111,79 +112,76 @@
                     <button type="submit" onclick="comment(<?php echo $current_post_id; ?>)">Comment</button>
                     
                     <hr>
+                    <!-- SHOW COMMENTS -->
                     <div id='<?php echo $all_comments_for_this; ?>'>
                         <?php
-
-                        if(count($arr_comments)>=$current_post_id)
+                        if(!isset($arr_comments[$current_post_id])) continue;
+                        for($j=0;$j<count($arr_comments[$current_post_id]); $j++)
                         {
-                           
-                            for($i=0;$i<count($arr_comments[$current_post_id]); $i++)
+                            echo '<h5>' . $arr_comments[$current_post_id][$j]['author'] . '</h5>';
+                            echo '<h6>' . $arr_comments[$current_post_id][$j]['body'] . '</h6>';
+                            //check owner then add edit and delete options
+                            
+                            if(intval($arr_comments[$current_post_id][$j]['author'])==$_SESSION['id'])
                             {
-                                echo '<h5>' . $arr_comments[$current_post_id][$i]['author'] . '</h5>';
-                                echo '<h6>' . $arr_comments[$current_post_id][$i]['body'] . '</h6>';
-                                //check owner then add edit and delete options
-                                
-                                if(intval($arr_comments[$current_post_id][$i]['author'])==$_SESSION['id'])
+                                ?>
+                                <h6> 
+                                    <a href=
+                                    <?php echo '/ekom/public/comment/editComment/'.
+                                    $arr_comments[$current_post_id][$j]['id'];?>
+                                    >Edit</a> or 
+                                    <p style='cursor:pointer;' onclick=
+                                    '<?php 
+                                        echo 'deleteComment(' . $current_post_id . 
+                                        ', ' . $arr_comments[$current_post_id][$j]['id']
+                                        .')';
+                                    ?>'>Delete</p> 
+                                </h6> 
+                                <br>
+                                <?php
+                            // }
+                            // else {
+                                $arr_up_voters = explode(';', $arr_comments[$current_post_id][$j]['up_voters'], -1);
+
+                                if(in_array($_SESSION['id'], $arr_up_voters))
                                 {
-                                    ?>
-                                    <h6> 
-                                        <a href=
-                                        <?php echo '/ekom/public/comment/editComment/'.
-                                        $arr_comments[$current_post_id][$i]['id'];?>
-                                        >Edit</a> or 
-                                        <p style='cursor:pointer;' onclick=
-                                        '<?php 
-                                            echo 'deleteComment(' . $current_post_id . 
-                                            ', ' . $arr_comments[$current_post_id][$i]['id']
-                                            .')';
-                                        ?>'>Delete</p> 
-                                    </h6> 
-                                    <br>
-                                    <?php
-                                // }
-                                // else {
-                                    $arr_up_voters = explode(';', $arr_comments[$current_post_id][$i]['up_voters'], -1);
-
-                                    if(in_array($_SESSION['id'], $arr_up_voters))
-                                    {
-                                    ?>
-                                        <h6 id=<?php echo 'upvote_'. $arr_comments[$current_post_id][$i]['id']; ?> 
-                                        onclick=<?php echo 'upvote(' . $arr_comments[$current_post_id][$i]['id'] . 
-                                        ',)';?> style='cursor:pointer'> upvoted </h6>
-                                    
-                                    <?php
-                                    }
-                                    else {
-                                        ?>
-                                        <h6 id=<?php echo 'upvote_'. $arr_comments[$current_post_id][$i]['id']; ?> 
-                                        onclick=<?php echo 'upvote(' . $arr_comments[$current_post_id][$i]['id'] . 
-                                        ')';?> style='cursor:pointer'> upvote </h6>
-                                    <?php
-                                    }
-
-                                    $arr_down_voters = explode(';', $arr_comments[$current_post_id][$i]['down_voters'], -1);
-
-                                    if(in_array($_SESSION['id'], $arr_down_voters))
-                                    {
-                                    ?>
-                                        <h6 id=<?php echo 'downvote_'. $arr_comments[$current_post_id][$i]['id']; ?> 
-                                        onclick=<?php echo 'downvote(' . $arr_comments[$current_post_id][$i]['id'] . 
-                                        ',)';?> style='cursor:pointer'> downvoted </h6>
-                                    
-                                    <?php
-                                    }
-                                    else {
-                                        ?>
-                                        <h6 id=<?php echo 'downvote_'. $arr_comments[$current_post_id][$i]['id']; ?> 
-                                        onclick=<?php echo 'downvote(' . $arr_comments[$current_post_id][$i]['id'] . 
-                                        ')';?> style='cursor:pointer'> downvote </h6>
-                                    <?php
-                                    }
+                                ?>
+                                    <h6 id=<?php echo 'upvote_'. $arr_comments[$current_post_id][$j]['id']; ?> 
+                                    onclick=<?php echo 'upvote(' . $arr_comments[$current_post_id][$j]['id'] . 
+                                    ',)';?> style='cursor:pointer'> upvoted </h6>
+                                
+                                <?php
                                 }
-                                echo '<br>';
+                                else {
+                                    ?>
+                                    <h6 id=<?php echo 'upvote_'. $arr_comments[$current_post_id][$j]['id']; ?> 
+                                    onclick=<?php echo 'upvote(' . $arr_comments[$current_post_id][$j]['id'] . 
+                                    ')';?> style='cursor:pointer'> upvote </h6>
+                                <?php
+                                }
+
+                                $arr_down_voters = explode(';', $arr_comments[$current_post_id][$j]['down_voters'], -1);
+
+                                if(in_array($_SESSION['id'], $arr_down_voters))
+                                {
+                                ?>
+                                    <h6 id=<?php echo 'downvote_'. $arr_comments[$current_post_id][$j]['id']; ?> 
+                                    onclick=<?php echo 'downvote(' . $arr_comments[$current_post_id][$j]['id'] . 
+                                    ',)';?> style='cursor:pointer'> downvoted </h6>
+                                
+                                <?php
+                                }
+                                else {
+                                    ?>
+                                    <h6 id=<?php echo 'downvote_'. $arr_comments[$current_post_id][$j]['id']; ?> 
+                                    onclick=<?php echo 'downvote(' . $arr_comments[$current_post_id][$j]['id'] . 
+                                    ')';?> style='cursor:pointer'> downvote </h6>
+                                <?php
+                                }
                             }
+                            echo '<br>';
                         }
-                        
+
                         ?>
                     </div>
                     <hr>
